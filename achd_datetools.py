6a773d4fdc9d6628490cbf203e2a0ca1599b3bbd
kdurril <kdurril@gmail.com>
2017-11-24 22:04:40 -0500

    removed type annotations for Jython

--- achd_datetools.py
+++ achd_datetools.py
@@ -13,21 +13,21 @@ from sys import argv
 #ISO style
 #20170709
 
-def date_iso(d: dt.datetime) -> str:
+def date_iso(d):
     "convert a date object to achd formating"
     # datetime.date(2017, 10, 21) -> '20171021' 
     # alternative to '{:%Y%m%d}'.format(date_obj)
     # return d.isoformat().replace('-','')
     return d.strftime("%Y%m%d")
 
-def date_iter(start=dt.date.today(), end=dt.date.today()+dt.timedelta(days=1)) -> dt.date:
+def date_iter(start=dt.date.today(), end=dt.date.today()+dt.timedelta(days=1)):
     "date generator"
     change = dt.timedelta(days=1)
     while start < end:
         yield (start-change) + change
         start = start + change
 
-def date_gen(start=(2017,8,17), end=(2017,8,18)) -> dt.date:
+def date_gen(start=(2017,8,17), end=(2017,8,18)):
     "datetime counter"
     # takes tuples of ints that will be converted to a date object
     start = dt.date(*start)
