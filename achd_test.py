66ca2fee5972b14f962c2b230e31066ce66340a3
kdurril <kdurril@gmail.com>
2017-11-24 22:05:55 -0500

    command_dir function to set base directory and target directory

--- achd_test.py
+++ achd_test.py
@@ -1,38 +1,27 @@
 #!/usr/bin/jython
 # -*- coding: utf-8 -*-
 #
+from achd_datetools import achd_today
 from achd_pdfbox1229 import *
-from os import path
 import datetime as dt
+from glob import iglob
+from os import path
+from sys import argv
 
-base_project='./'
-input_date=dt.datetime.today()
-day = '{:%Y%m%d}'.format(input_date)
-#day = '{:%Y%m%d}'.format(dt.datetime(2017,7,20))
-
-dir_out=glob.iglob(base_project+day+'/'+day+'*.pdf')
-for x in dir_out:
-    basename=path.basename(x)
-    jsonfile = base_project+'json/'+basename[:-4]+'.json'
-    txtfile = base_project+'txt/'+basename[:-4]+'.txt'
-    try:
-        pdf_obj=achdInspect(x)
-        jsonify(jsonfile,pdf_obj.doc_label_ALT())
-        base_text(txtfile,pdf_obj)
-    except:
-        print(pdf_obj.filename)
-if __name__ == '__main__':
-    from itertools import chain
-    days = ['{:%Y%m%d}'.format(dt.datetime(2017,7,y)) for y in range(21,21)]
-    dir_out=(glob.iglob(base_project+day+'/'+day+'*.pdf') for day in days)
-    for x in chain.from_iterable(dir_out):
+def current_dir(base_dir="/mnt/", directory="20171010/"):
+#    if directory[-1] != '/':
+#        directory + '/'
+    dir_out=list(iglob(base_dir+directory+"*.pdf"))
+    for x in dir_out:
         basename=path.basename(x)
-        jsonfile = base_project+'json/'+basename[:-4]+'.json'
-        txtfile = base_project+'txt/'+basename[:-4]+'.txt'
+        jsonfile = base_dir+'json/'+basename[:-4]+'.json'
+        txtfile = base_dir+'txt/'+basename[:-4]+'.txt'
         try:
-           pdf_obj=achdInspect(x)
-           jsonify(jsonfile,pdf_obj.doc_label_ALT())
-           base_text(txtfile,pdf_obj)
+            pdf_obj=achdInspect(x)
+            jsonify(jsonfile,pdf_obj.doc_label_ALT())
+            base_text(txtfile,pdf_obj)
         except:
-           print(pdf_obj.filename)
-
+            print("C")
+if __name__ == '__main__':
+    #d = dt.datetime.strptime(argv[1], '%Y%m%d')
+    current_dir(base_dir="", directory=achd_today+"/")
